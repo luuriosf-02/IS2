8bf108c663b9522fca48cd8cef21a3ab72515046
fabR4M-star <260748508+fabR4M-star@users.noreply.github.com>
2026-08-31 09:40:46 -0300

    Mejora UI y simplifica registros

--- app/apps/clientes/urls.py
+++ app/apps/clientes/urls.py
@@ -5,5 +5,7 @@ app_name = 'clientes'
 
 urlpatterns = [
     path('', views.lista_clientes, name='lista'),
+    path('crear/', views.crear_cliente, name='crear'),
     path('<int:pk>/', views.detalle_cliente, name='detalle'),
+    path('<int:pk>/editar/', views.editar_cliente, name='editar'),
 ]
